ee2c39ca2401bb3c88140479aa09eb40dcfa2a1c
kushai <lkushai5b@gmail.com>
2024-10-23 19:12:31 +0100

    Update Math-Functions.lua

--- Math-Functions.lua
+++ Math-Functions.lua
@@ -17,3 +17,14 @@ local function ChanceNegativeRandom(Min,Max)
 		return RandomNum
 	end
 end
+
+function module.BezierCurve(Point1, Point2, Point3, Point4, Time)
+	local Line1 = Point1 + (Point2 - Point1) * Time
+	local Line2 = Point2 + (Point3 - Point2) * Time
+	local Line3 = Point3 + (Point4 - Point3) * Time
+
+	local Curve1 = Line1 + (Line2 - Line1) * Time
+	local Curve2 = Line2 + (Line3 - Line2) * Time
+
+	return Curve1 + (Curve2 - Curve1) * Time
+end
